0a12ee377ef41bb37101323e641d877c15463d8f
brummer10 <brummer-@web.de>
2018-12-04 06:05:20 +0100

    fix keyboard event handling

--- gui/gx_voxbass_x11ui.c
+++ gui/gx_voxbass_x11ui.c
@@ -211,12 +211,12 @@ static LV2UI_Handle instantiate(const struct _LV2UI_Descriptor * descriptor,
 		return NULL;
 	}
 
-	ui->controls[0] = { 0.5, 0.5, 0.0, 1.0, 0.01, 50, 60, 61, 61, false,"VOLUME", KNOB, VOLUME};
-	ui->controls[1] = { 0.5, 0.5, 0.0, 1.0, 0.01, 120, 60, 61, 61, false,"BASS", KNOB, BASS};
-	ui->controls[2] = { 0.5, 0.5, 0.0, 1.0, 0.01, 190, 60, 61, 61, false,"MID", KNOB, MID};
-	ui->controls[3] = { 0.5, 0.5, 0.0, 1.0, 0.01, 260, 60, 61, 61, false,"TREBLE", KNOB, TREBLE};
-	ui->controls[4] = { 0.0, 0.0, 0.0, 1.0, 1.0, 370, 60, 61, 61, false,"CABINET", SWITCH, CAB};
-	ui->controls[5] = { 1.0, 1.0, 0.0, 1.0, 1.0, 550, 60, 61, 61, false,"POWER", BSWITCH, BYPASS};
+	ui->controls[0] = (gx_controller) {{ 0.5, 0.5, 0.0, 1.0, 0.01}, { 50, 60, 61, 61}, false,"VOLUME", KNOB, VOLUME};
+	ui->controls[1] = (gx_controller) {{ 0.5, 0.5, 0.0, 1.0, 0.01}, { 120, 60, 61, 61}, false,"BASS", KNOB, BASS};
+	ui->controls[2] = (gx_controller) {{ 0.5, 0.5, 0.0, 1.0, 0.01}, { 190, 60, 61, 61}, false,"MID", KNOB, MID};
+	ui->controls[3] = (gx_controller) {{ 0.5, 0.5, 0.0, 1.0, 0.01}, { 260, 60, 61, 61}, false,"TREBLE", KNOB, TREBLE};
+	ui->controls[4] = (gx_controller) {{ 0.0, 0.0, 0.0, 1.0, 1.0},  { 370, 60, 61, 61}, false,"CABINET", SWITCH, CAB};
+	ui->controls[5] = (gx_controller) {{ 1.0, 1.0, 0.0, 1.0, 1.0},  { 550, 60, 61, 61}, false,"POWER", BSWITCH, BYPASS};
 
 
 	ui->pedal = cairo_image_surface_create_from_stream(ui, LDVAR(pedal_png));
@@ -759,6 +759,46 @@ static void motion_event(gx_voxbassUI *ui, double start_value, XMotionEvent *mo)
 	
 }
 
+// map supported key's to integers or return zerro
+static int key_mapping(Display *dpy, XKeyEvent *xkey) {
+	if (xkey->keycode == XKeysymToKeycode(dpy,XK_Tab))
+		return (xkey->state == ShiftMask) ? 1 : 2;
+	else if (xkey->keycode == XKeysymToKeycode(dpy,XK_Up))
+		return 3;
+	else if (xkey->keycode == XKeysymToKeycode(dpy,XK_Right))
+		return 3;
+	else if (xkey->keycode == XKeysymToKeycode(dpy,XK_Down))
+		return 4;
+	else if (xkey->keycode == XKeysymToKeycode(dpy,XK_Left))
+		return 4;
+	else if (xkey->keycode == XKeysymToKeycode(dpy,XK_Home))
+		return 5;
+	else if (xkey->keycode == XKeysymToKeycode(dpy,XK_Insert))
+		return 6;
+	else if (xkey->keycode == XKeysymToKeycode(dpy,XK_End))
+		return 7;
+	// keypad
+	else if (xkey->keycode == XKeysymToKeycode(dpy,XK_KP_Subtract))
+		return 1;
+	else if (xkey->keycode == XKeysymToKeycode(dpy,XK_KP_Add))
+		return 2;
+	else if (xkey->keycode == XKeysymToKeycode(dpy,XK_KP_Up))
+		return 3;
+	else if (xkey->keycode == XKeysymToKeycode(dpy,XK_KP_Right))
+		return 3;
+	else if (xkey->keycode == XKeysymToKeycode(dpy,XK_KP_Down))
+		return 4;
+	else if (xkey->keycode == XKeysymToKeycode(dpy,XK_KP_Left))
+		return 4;
+	else if (xkey->keycode == XKeysymToKeycode(dpy,XK_KP_Home))
+		return 5;
+	else if (xkey->keycode == XKeysymToKeycode(dpy,XK_KP_Insert))
+		return 6;
+	else if (xkey->keycode == XKeysymToKeycode(dpy,XK_KP_End))
+		return 7;
+	else return 0;
+}
+
 // general xevent handler
 static void event_handler(gx_voxbassUI *ui) {
 	XEvent xev;
@@ -818,26 +858,26 @@ static void event_handler(gx_voxbassUI *ui) {
 			break;
 
 			case KeyPress:
-				if ((xev.xkey.state == ShiftMask) &&
-				  (xev.xkey.keycode == XKeysymToKeycode(ui->dpy,XK_Tab)))
-					set_previous_controller_active(ui);
-				else if (xev.xkey.keycode == XKeysymToKeycode(ui->dpy,XK_Tab))
-					set_next_controller_active(ui);
-				else if (xev.xkey.keycode == XKeysymToKeycode(ui->dpy,XK_Up))
-					key_event(ui, 1);
-				else if (xev.xkey.keycode == XKeysymToKeycode(ui->dpy,XK_Right))
-					key_event(ui, 1);
-				else if (xev.xkey.keycode == XKeysymToKeycode(ui->dpy,XK_Down))
-					key_event(ui, -1);
-				else if (xev.xkey.keycode == XKeysymToKeycode(ui->dpy,XK_Left))
-					key_event(ui, -1);
-				else if (xev.xkey.keycode == XKeysymToKeycode(ui->dpy,XK_Home))
-					set_key_value(ui, 1);
-				else if (xev.xkey.keycode == XKeysymToKeycode(ui->dpy,XK_Insert))
-					set_key_value(ui, 2);
-				else if (xev.xkey.keycode == XKeysymToKeycode(ui->dpy,XK_End))
-					set_key_value(ui, 3);
+				switch (key_mapping(ui->dpy, &xev.xkey)) {
+					case 1: set_previous_controller_active(ui);
+					break;
+					case 2: set_next_controller_active(ui);
+					break;
+					case 3: key_event(ui, 1);
+					break;
+					case 4: key_event(ui, -1);
+					break;
+					case 5: set_key_value(ui, 1);
+					break;
+					case 6: set_key_value(ui, 2);
+					break;
+					case 7: set_key_value(ui, 3);
+					break;
+					default:
+					break;
+				}
 			break;
+
 			case EnterNotify:
 				if (!blocked) get_active_controller(ui, true);
 			break;
@@ -846,7 +886,7 @@ static void event_handler(gx_voxbassUI *ui) {
 			break;
 			case MotionNotify:
 				// mouse move while button1 is pressed
-				if(xev.xmotion.state == Button1MotionMask) {
+				if(xev.xmotion.state & Button1Mask) {
 					motion_event(ui, start_value, &xev.xmotion);
 				}
 			break;
@@ -896,7 +936,6 @@ static const LV2UI_Descriptor descriptor = {
 	extension_data
 };
 
-extern "C"
 LV2_SYMBOL_EXPORT
 const LV2UI_Descriptor* lv2ui_descriptor(uint32_t index) {
 	switch (index) {
