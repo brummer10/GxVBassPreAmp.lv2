3cee8074e85efe9c01206576108c94cc9f6e4db3
brummer10 <brummer-@web.de>
2019-10-01 06:58:35 +0200

    insert guard to prevent event loop

--- gui/gx_voxbass_x11ui.c
+++ gui/gx_voxbass_x11ui.c
@@ -146,6 +146,7 @@ typedef struct {
 	cairo_surface_t *surface;
 
 	gx_controller controls[CONTROLS];
+	int block_event;
 	gx_scale rescale;
 
 	void *controller;
@@ -217,6 +218,7 @@ static LV2UI_Handle instantiate(const struct _LV2UI_Descriptor * descriptor,
 	ui->controls[3] = (gx_controller) {{ 0.5, 0.5, 0.0, 1.0, 0.01}, { 260, 60, 61, 61}, false,"TREBLE", KNOB, TREBLE};
 	ui->controls[4] = (gx_controller) {{ 0.0, 0.0, 0.0, 1.0, 1.0},  { 370, 60, 61, 61}, false,"CABINET", SWITCH, CAB};
 	ui->controls[5] = (gx_controller) {{ 1.0, 1.0, 0.0, 1.0, 1.0},  { 550, 60, 61, 61}, false,"POWER", BSWITCH, BYPASS};
+	ui->block_event = -1;
 
 
 	ui->pedal = cairo_image_surface_create_from_stream(ui, LDVAR(pedal_png));
@@ -558,8 +560,10 @@ static void send_controller_event(gx_voxbassUI *ui, int controller) {
 static void check_value_changed(gx_voxbassUI *ui, int i, float* value) {
 	if(fabs(*(value) - ui->controls[i].adj.value)>=0.00001) {
 		ui->controls[i].adj.value = *(value);
-		ui->write_function(ui->controller,ui->controls[i].port,sizeof(float),0,value);
+		if (ui->block_event != ui->controls[i].port)
+			ui->write_function(ui->controller,ui->controls[i].port,sizeof(float),0,value);
 		send_controller_event(ui, i);
+		ui->block_event = -1;
 	}
 }
 
@@ -588,6 +592,7 @@ static void port_event(LV2UI_Handle handle, uint32_t port_index,
 	float value = *(float*)buffer;
 	for (int i=0;i<CONTROLS;i++) {
 		if (port_index == ui->controls[i].port) {
+			ui->block_event = (int)port_index;
 			check_value_changed(ui, i, &value);
 		}
 	}
