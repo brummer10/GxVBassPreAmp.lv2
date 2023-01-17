2fd32920bd43b284d02958f7f581eb8e10fb1ed7
Thorsten Maerz <info@netztorte.de>
2023-01-17 14:07:31 +0100

    MSWin: correct name passed to UnregisterClass()

--- gui/gx_platform_mswin.c
+++ gui/gx_platform_mswin.c
@@ -71,7 +71,7 @@ void gx_gui_register_controller_message(gx_voxbassUI *ui) {
 void gx_gui_destroy_main_window(gx_voxbassUI *ui) {
 	DestroyWindow(ui->win);
 	// safe to use: doesnt unregister if there are still windows of this class
-	UnregisterClass(TEXT("gx_DrawSurfaceClass"), NULL);
+	UnregisterClass(TEXT("gx_voxbassUIClass"), NULL);
 }
 
 void gx_gui_resize_surface(gx_voxbassUI *ui) {
